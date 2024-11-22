64379974a964dfc915253ee415c1f99a54c66cf5
Agent61 <59861986+hanintech@users.noreply.github.com>
2024-11-22 13:26:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
